0708ad3059cd51a31b381f75f0a1bb514f6f9d99
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-04-10 14:02:08 +0530

    API links updated

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 export const fetchInventory = () => async (dispatch) => {
     try{
         dispatch({type: "FETCH_DATA_LOADING"});
-        const response= await fetch("https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/items")
+        const response= await fetch("https://inventory-management-app-ebon.vercel.app/items")
         const data = await response.json();
         dispatch({type: "FETCH_INVENTORY_SUCCESS", payload: data })
     }
@@ -14,7 +14,7 @@ export const fetchInventory = () => async (dispatch) => {
 export const fetchSales = () => async (dispatch) => {
     try{
         dispatch({type: "FETCH_DATA_LOADING"})
-        const response = await fetch("https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/sales")
+        const response = await fetch("https://inventory-management-app-ebon.vercel.app/sales")
         const data = await response.json();
         dispatch({type: "FETCH_SALES_SUCCESS", payload: data})
     }
@@ -26,7 +26,7 @@ export const fetchSales = () => async (dispatch) => {
 
 export const addInventory = (newEntry) => async (dispatch) => {
     try{
-     const response = await fetch("https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/items",{
+     const response = await fetch("https://inventory-management-app-ebon.vercel.app/items",{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ export const addInventory = (newEntry) => async (dispatch) => {
 
 export const deleteInventory = (entry) => async (dispatch) => {
     try{
-        const response = await fetch(`https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/items/${entry}`,{
+        const response = await fetch(`https://inventory-management-app-ebon.vercel.app/items/${entry}`,{
             method: "DELETE",
             headers: {
                 'Content-Type': 'application/json'
@@ -65,7 +65,7 @@ export const deleteInventory = (entry) => async (dispatch) => {
 
 export const updateInventory = (entry) => async (dispatch) => {
     try{
-        const response = await fetch(`https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/items/${entry.id}`,{
+        const response = await fetch(`https://inventory-management-app-ebon.vercel.app/items/${entry.id}`,{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -85,7 +85,7 @@ export const updateInventory = (entry) => async (dispatch) => {
 
 export const addSales = (newEntry) => async (dispatch) => {
     try{
-        const response = await fetch("https://0eeee39d-60ad-4daa-9bd2-ee49b469d77e-00-1w6ttqoj05hfx.spock.replit.dev/sales", 
+        const response = await fetch("https://inventory-management-app-ebon.vercel.app/sales", 
         {
             method: "POST",
             headers: {
